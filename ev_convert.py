fdf8e5ff0b1f680f43aa14692d3d41960c66889c
BryceDixon <bryce.dixon.bd@gmail.com>
2023-10-16 16:13:30 +0100

    added contrast curve converters

--- ev_convert.py
+++ ev_convert.py
@@ -9,7 +9,7 @@ from scipy.interpolate import interp1d as interp
 
 class contrast_convert:
     
-    def __init__(self, age, mag, mass, save_folder = None, savefilename = None):
+    def __init__(self, age, mag, mass, save_folder = None):
         """Class to convert a stellar evolutionary model to a contrast curve
         
         Parameters
@@ -20,12 +20,15 @@ class contrast_convert:
             array of the magnitude data for all tracks in the form n columns = mass, m rows = age, (one track should be one whole column)
         mass : list of floats
             list of the masses for each track in the same order as the mag array
+        save_folder : str, optional
+            directory of the folder to save the plots to
         """
         self.age = age
         self.mag = mag
         self.mass = mass
+        self.save_folder = save_folder
         
-    def ev_plot(self):
+    def ev_plot(self, savefilename = None):
         """Function to plot the stellar evolutionary models
         """
     
@@ -58,14 +61,14 @@ class contrast_convert:
         ax.set_title('Stellar Evolutionary Models')
     
         if self.save_folder is not None:
-            assert self.savefilename is not None, "You need to give both save_folder and savefilename to save the figure"
-            plt.savefig(str(self.save_folder)+"/"+str(self.savefilename)+".png", bbox_inches='tight')
+            assert savefilename is not None, "You need to give both save_folder and savefilename to save the figure"
+            plt.savefig(str(self.save_folder)+"/"+str(savefilename)+".png", bbox_inches='tight')
 
-        if self.savefilename is not None and self.save_folder is None:
+        if savefilename is not None and self.save_folder is None:
             print("Figure not saved, you need to provide savefilename and save_folder both")
     
     
-    def ev_convert(self, age_track):
+    def ev_convert(self, age_track, savefilename = None):
         """Function to convert the evolutionary model to a plot of mass against magnitude for a given age
 
         Parameters
@@ -124,14 +127,14 @@ class contrast_convert:
         self.age_track = age_track
         
         if self.save_folder is not None:
-            assert self.savefilename is not None, "You need to give both save_folder and savefilename to save the figure"
-            plt.savefig(str(self.save_folder)+"/"+str(self.savefilename)+".png", bbox_inches='tight')
+            assert savefilename is not None, "You need to give both save_folder and savefilename to save the figure"
+            plt.savefig(str(self.save_folder)+"/"+str(savefilename)+".png", bbox_inches='tight')
 
-        if self.savefilename is not None and self.save_folder is None:
+        if savefilename is not None and self.save_folder is None:
             print("Figure not saved, you need to provide savefilename and save_folder both")
 
 
-    def mag_to_mass(self, mag_data, sep_data):
+    def mag_to_mass(self, mag_data, sep_data, stellar_mag, distance, savefilename = None):
         """Function to take the absolute magnitude and separation data from a contrast plot and convert the absolute magnitudes to a mass then plot mass against seperation
 
         Parameters
@@ -140,24 +143,123 @@ class contrast_convert:
             array of absolute magnitudes from the contrast data
         sep_data : array of floats
             array of separations in AU from the contrast data
+        stellar_mag : float
+            absolute magnitude of the star
+        distance : float
+            distance to the target in parsecs
         """
         
         f = interp(self.y, self.x, kind = 'cubic')
-        mass_data = f(mag_data)
+        #mass_data = f(mag_data)
+        mass_data = []
+        new_sep_data = []
+        for i in range(len(mag_data)):
+            try:
+                mass_data.append(f(mag_data[i]))
+                new_sep_data.append(sep_data[i])
+            except:
+                pass
         
         fig = plt.figure(figsize=(10, 10))
         ax = fig.add_subplot(1,1,1)
         
-        ax.plot(sep_data, mass_data)
+        ax.plot(new_sep_data, mass_data)
         
-        ax.set_yscale('log')
         ax.set_ylabel('Mass ($M_s$)')
         ax.set_xlabel('Separation (AU)')
-        ax.set_title('Mass against Separation for a {} Gyrs Star'.format(self.age_track))
+        ax.set_title('Mass contrast curve for a {} Gyrs, {} Mag Star at a distance of {} pc'.format(self.age_track, stellar_mag, distance))
         
         if self.save_folder is not None:
-            assert self.savefilename is not None, "You need to give both save_folder and savefilename to save the figure"
-            plt.savefig(str(self.save_folder)+"/"+str(self.savefilename)+".png", bbox_inches='tight')
+            assert savefilename is not None, "You need to give both save_folder and savefilename to save the figure"
+            plt.savefig(str(self.save_folder)+"/"+str(savefilename)+".png", bbox_inches='tight')
 
-        if self.savefilename is not None and self.save_folder is None:
+        if savefilename is not None and self.save_folder is None:
             print("Figure not saved, you need to provide savefilename and save_folder both")
+
+
+
+def convert(age, mag, mass, age_track, mag_data, sep_data, stellar_mag, distance, save_folder = None):
+    """Function to plot the given evolutionary model, then convert the absolute magnitudes of a given contrast curve to masses to plot a curve of mass against seperation for a given age
+
+    Parameters
+    ----------
+    age : list of floats
+        list of the ages for each point in the same order as the mag array
+    mag : array of floats
+        array of the magnitude data for all tracks in the form n columns = mass, m rows = age, (one track should be one whole column)
+    mass : list of floats
+        list of the masses for each track in the same order as the mag array
+    save_folder : str, optional
+        directory of the folder to save the plots to
+    age_track : float
+        desired stellar age to plot the mass-magnitude relationship for in Gyrs
+    mag_data : array of floats
+        array of absolute magnitudes from the contrast data
+    sep_data : array of floats
+        array of separations in AU from the contrast data
+    stellar_mag : float
+        absolute magnitude of the star
+    distance : float
+        distance to the target in parsecs
+    save_folder : str, optional
+        directory of the folder to save the plots to, defaults to None
+    """
+    
+    cont = contrast_convert(age, mag, mass, save_folder)
+    cont.ev_plot('evolutionary_plot')
+    cont.ev_convert(age_track, 'mag_vs_mass_plot')
+    cont.mag_to_mass(mag_data, sep_data, stellar_mag, distance, 'mass_vs_sep_plot')
+    
+
+
+
+def contrast_to_mag(stellar_mag, contrasts):
+    """Function to convert an array of contrasts to an array of magnitudes for a given stellar magnitude
+
+    Parameters
+    ----------
+    stellar_mag : float
+        absolute magnitude of the star
+    contrasts : array of floats
+        array of contrasts to be converted to magnitudes
+    zero_point_lum : float, optional
+        zero point luminosity, defaults to 3.0128e+28
+    
+    Returns
+    -------
+    M_array : array of floats
+        array of absolute magnitudes converted from the contrasts
+    """
+    
+    contrasts = np.array(contrasts)
+    zero_point_lum = 3.0128e+28
+    
+    L_star = zero_point_lum * 10 ** (-stellar_mag/2.5)
+    L_array = L_star * contrasts
+    M_array = -2.5 * np.log10(L_array/zero_point_lum)
+    
+    return M_array
+
+
+
+def arc_to_au(arcseconds, distance):
+    """Function to convert an array of separations in arcseconds to an array of separations in AU for a given distance
+
+    Parameters
+    ----------
+    arcseconds : array of floats
+        array of separations in arcseconds
+    distance : float
+        distance to the target in parsecs
+    
+    Returns 
+    -------
+    AU_array : array of floats
+        array of separations in AU
+    """
+    
+    arcseconds = np.array(arcseconds)
+    
+    AU_array = distance * arcseconds
+    
+    return AU_array
